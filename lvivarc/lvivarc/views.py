bd299dcd1d4886a1ad6c3cad4297c4e3ab5628a1
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-09-12 14:17:42 +0300

    Revert "init"
    This reverts commit 1fcc8c04f2048aa2c26dc9238d84dd9d8433a178.

--- lvivarc/lvivarc/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-
-def homepage(request: HttpRequest) -> HttpResponse:
-    return render(request, 'index.html')
