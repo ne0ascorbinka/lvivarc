a04cdf13fc0d4fe39cfc47aadb28a5473cdea699
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-09-23 22:57:04 +0300

    add arcobject model

--- lvivarc/architecture/views.py
+++ lvivarc/architecture/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
-from .models import ArchitectureGroup
+from .models import ArchitectureGroup, ArchitectureObject
 
 # Create your views here.
 def group_page(request: HttpRequest, slug: str) -> HttpResponse:
     group = get_object_or_404(ArchitectureGroup, slug=slug)
-    return render(request, 'group.html', context={"group": group})
+    arc_objects = group.arc_objects.all()
+    return render(request, 'group.html', context={"group": group, "places": arc_objects})
+
+def object_page(request: HttpRequest, pk: int, slug) -> HttpResponse:
+    arc_object = get_object_or_404(ArchitectureObject, pk=pk)
+    return render(request, 'object.html', context={"place": arc_object})
